39239927d9a09d22b8e73c4b7694f07ae725c211
Waqas Khan <waqas.k@live.com>
2017-02-19 02:54:13 +0500

    Cutom Toolbar added.

--- CampusRecruitmentSystem/app/src/main/java/com/waqkz/campusrecruitmentsystem/AccountInfoFlow/AccountInfoActivity.java
+++ CampusRecruitmentSystem/app/src/main/java/com/waqkz/campusrecruitmentsystem/AccountInfoFlow/AccountInfoActivity.java
@@ -1,21 +1,33 @@
 package com.waqkz.campusrecruitmentsystem.AccountInfoFlow;
 
 import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
 
 import com.waqkz.campusrecruitmentsystem.R;
 
 public class AccountInfoActivity extends AppCompatActivity {
 
-    private String memberShipType;
+    private String membershipType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_info);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        Window window = getWindow();
+
+        if (Build.VERSION.SDK_INT >= 21)
+            window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+
         Intent intent = getIntent();
-        memberShipType = intent.getExtras().getString("memberType");
+        membershipType = intent.getExtras().getString("memberType");
     }
 }
